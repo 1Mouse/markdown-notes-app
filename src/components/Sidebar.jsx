311dbcad2556a70f22e0a6e67f1ce9529c2267ce
Mohamed Elfar <melfar391@gmail.com>
2022-10-22 01:46:34 +0200

    main features finished successfully

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,36 @@
+import React from "react"
+
+export default function Sidebar(props) {
+    const getSummary = (note) => {
+        return note.body.split(/\r?\n/)[0];
+    };
+
+    const noteElements = props.notes.map((note, index) => (
+        <div key={note.id}>
+            <div
+
+                className={`title ${
+                    note.id === props.currentNote.id ? "selected-note" : ""
+                    }`}
+                onClick={() => props.setCurrentNoteId(note.id)}
+            >
+                <h4 className="text-snippet">{getSummary(note)}</h4>
+                <button
+                    className="delete-btn"
+                    onClick={(event) => props.deleteNote(event, note.id)}                >
+                    <i className="gg-trash trash-icon"></i>
+                </button>
+            </div>
+        </div>
+    ));
+
+    return (
+        <section className="pane sidebar">
+            <div className="sidebar--header">
+                <h3>Notes</h3>
+                <button className="new-note" onClick={props.newNote}>+</button>
+            </div>
+            {noteElements}
+        </section>
+    );
+}
